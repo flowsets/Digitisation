a131154ffeed629632064f1614ac638754e8013c
badeacristian <50447697+badeacristian@users.noreply.github.com>
2019-05-12 07:25:27 +0100

    Add files via upload

---
+++ JpgtoPdf  V1 (Cristian)/JpgtoPdf demo1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace JpgtoPdf_demo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.JpgtoPdf = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // JpgtoPdf
+            // 
+            this.JpgtoPdf.Location = new System.Drawing.Point(58, 53);
+            this.JpgtoPdf.Name = "JpgtoPdf";
+            this.JpgtoPdf.Size = new System.Drawing.Size(105, 69);
+            this.JpgtoPdf.TabIndex = 0;
+            this.JpgtoPdf.Text = "JpgtoPdf";
+            this.JpgtoPdf.UseVisualStyleBackColor = true;
+            this.JpgtoPdf.Click += new System.EventHandler(this.JpgtoPdf_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(196, 450);
+            this.Controls.Add(this.JpgtoPdf);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button JpgtoPdf;
+    }
+}
+
---
+++ JpgtoPdf  V1 (Cristian)/JpgtoPdf demo1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JpgtoPdf_demo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void JpgtoPdf_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.InitialDirectory = "C:\\PdfGenerator";
+            dlg.Filter = "JPEG files (*.jpg) |*.jpg|All files (*.*)|*.*";
+            dlg.Multiselect = true;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                PdfDocument document = new PdfDocument();
+                document.Info.Title = "Created using Pdf Sharp";
+
+                foreach (string fileSpec in dlg.FileNames)
+                {
+                    PdfPage page = document.AddPage();
+                    XGraphics gfx = XGraphics.FromPdfPage(page);
+                    DrawImage(gfx, fileSpec, 0, 0, (int)page.Width, (int)page.Height);
+                }
+                if (document.PageCount > 0) document.Save(@"C:\PdfGenerator\Transform.pdf");
+            }
+        }
+
+        void DrawImage(XGraphics gfx, string jpegSamplePath, int x, int y, int width, int height)
+        {
+            XImage image = XImage.FromFile(jpegSamplePath);
+            gfx.DrawImage(image, x, y, width, height);
+        }
+    }
+}
